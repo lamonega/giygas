c41e3309c337ee31749fb03e887c70f00ee19ebd
Laureano F. Lamonega <laureanolamonega@gmail.com>
2025-05-01 19:28:41 -0300

    010525@lamonega

---
+++ Código/Rust/TP2/ej1.rs
@@ -0,0 +1,7 @@
+fn es_par(numero: i32) -> bool {
+    numero % 2 == 0
+}
+
+fn main () {
+    println!(es_par(12));
+}
---
+++ Código/Rust/TP2/ej2.rs
@@ -0,0 +1,11 @@
+fn es_primo(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
